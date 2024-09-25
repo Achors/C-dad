04de8af6c5a9c140820cf58b0042a71310daae09
Achors <adolfnyerere@gmail.com>
2024-09-25 23:02:46 +0200

    week-3 update

--- week-3/main.c
+++ week-3/main.c
@@ -1,17 +1,43 @@
-#include <stdio.h>      // for printf
-#include "functions.h"  // for read_int
+#include <stdio.h>
+#include "functions.h"
 
-int main(void) {
-    printf("Enter the number of rows: ");
-    int rows = read_int();
-    for (int i = rows; i >= 1; i--) {
-        // print the i-th row of the triangle
-        for (int j = i; j >= 1; j--) {
-            // print an asterisk
-            printf("%d", j);
-        }
-        printf("\n");   // newline after each row
+
+
+int main(void){
+    int count = 0;
+    int max_num = 0, second_high = 0;
+
+
+    printf("Enter a random number.");
+    int random_num = read_int();
+
+    while (random_num != 0) {
+        count++;        
+
+        if (random_num > max_num){
+            second_high = max_num;
+            max_num = random_num;
+            
+        } else if (random_num > second_high && random_num != max_num){
+            random_num = second_high;
+
+        } 
+        printf("Enter a random number.");
+        random_num = read_int();
+             
     }
+    
 
+    if (count < 2){
+            printf("Not enough numbers entered. \n");
+        }else if (random_num == random_num && random_num == max_num && random_num == second_high){
+            printf("Use different numbers. \n");
+            
+        } else {
+            printf("You entered %d numbers and %d is the second highest. \n", count, second_high); 
+
+        }
+       
+    
     return 0;
 }
