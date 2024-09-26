1a175d13c14213aed354798a00f673932c864976
Achors <adolfnyerere@gmail.com>
2024-09-26 16:50:43 +0200

    nun

--- week-3/main.c
+++ week-3/main.c
@@ -5,11 +5,11 @@
 
 int main(void){
     int count = 0;
-    int max_num = 0, second_high = 0;
+    float max_num = 0, second_high = 0, num_repeated = 0;
 
 
     printf("Enter a random number.");
-    int random_num = read_int();
+    float random_num = read_int();
 
     while (random_num != 0) {
         count++;        
@@ -17,9 +17,11 @@ int main(void){
         if (random_num > max_num){
             second_high = max_num;
             max_num = random_num;
+            num_repeated++;
             
         } else if (random_num > second_high && random_num != max_num){
             random_num = second_high;
+            num_repeated++;
 
         } 
         printf("Enter a random number.");
@@ -28,13 +30,13 @@ int main(void){
     }
     
 
-    if (count < 2){
+    if (count < 2 ){
             printf("Not enough numbers entered. \n");
-        }else if (random_num == random_num && random_num == max_num && random_num == second_high){
+        }else if (num_repeated < 2){
             printf("Use different numbers. \n");
             
         } else {
-            printf("You entered %d numbers and %d is the second highest. \n", count, second_high); 
+            printf("You entered %d numbers and %.2f is the second highest. \n", count, second_high); 
 
         }
        
